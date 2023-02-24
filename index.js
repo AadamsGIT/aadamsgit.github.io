43c3ca36affc56890c4434cb83171eb9f119e914
Alex <alexadams.ara@gmail.com>
2023-02-23 20:40:06 -0700

    updated pages

--- index.js
+++
@@ -1,9 +0,0 @@
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-ctx.beginPath();
-ctx.arc(95, 50, 40, 0, 2 * Math.PI);
-ctx.stroke();
-console.log("hello world");
-ctx.fillStyle = "#fff";
-ctx.fillRect(0, 0, c.width, c.height);
-document.body.style.background = "url(" + c.toDataURL() + ")";
